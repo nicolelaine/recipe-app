8b695e61e26f4a9d04c4de075cc134bfb6c497da
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2024-03-03 13:49:01 +0100

    Creating a put endpoint

--- api/api.py
+++ api/api.py
@@ -41,14 +41,14 @@ def get_all_recipes():
     return jsonify(recipe_list)
 
 
-@app.route('/api/recipes'. methods=['POST'])
+@app.route('/api/recipes', methods=['POST'])
 def add_recipe():
     data = request.get_json()
     required_fields = ['title', 'ingredients', 'instructions', 'servings', 'description', 'image_url']
     for field in required_fields:
         if field not in data or data[field] == "":
             return jsonify({'error': f"Missing required field: '{field}'"}), 400
-     new_recipe = Recipe(
+    new_recipe = Recipe(
         title=data['title'],
         ingredients=data['ingredients'],
         instructions=data['instructions'],
@@ -70,6 +70,39 @@ def add_recipe():
     }
 
     return jsonify({'message': 'Recipe added successfully', 'recipe': new_recipe_data})
+
+@app.route('/api/recipes/<int:recipe_id>')
+def update_recipe(recipe_id):
+    recipe = Recipe.query.get(recipe_id)
+    if not recipe:
+        return jsonify({'error': 'Recipe not found'}), 404
+    data = request.get_json()
+   # Validate the incoming JSON data for required fields
+    required_fields = ['title', 'ingredients',
+                      'instructions', 'servings', 'description', 'image_url']
+    for field in required_fields:
+        if field not in data or data[field] == "":
+            return jsonify({'error': f"Missing required field: '{field}'"}), 400
+    recipe.title = data['title']
+    recipe.ingredients = data['ingredients']
+    recipe.instructions = data['instructions']
+    recipe.servings = data['servings']
+    recipe.description = data['description']
+    recipe.image_url = data['image_url']
+    db.session.commit()
+ 
+    updated_recipe = {
+        'id': recipe.id,
+        'title': recipe.title,  
+        'ingredients': recipe.ingredients,
+        'instructions': recipe.instructions,
+        'servings': recipe.servings,
+        'description': recipe.description,
+        'image_url': recipe.image_url
+    }
+    return jsonify({'message': 'Recipe updated successfully', 'recipe': updated_recipe})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
