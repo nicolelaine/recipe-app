b6e248049166ea8ab4ea6d1eeabafaab3233d5f9
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2024-03-28 23:41:50 +0100

    Created abilty to add recipes via the form

--- src/App.js
+++ src/App.js
@@ -67,11 +67,50 @@ function App() {
      setNewRecipe({...newRecipe, [name]: value});
   };   
 
+
+  const handleNewRecipe = async (e, NewRecipeForm) => {
+    e.preventDefault()
+    try {
+   //   setLoading(true)
+      const response = await fetch('api/recipes', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(NewRecipeForm)
+      });
+        if (response.status === 200) {
+           const data =  await response.json()
+           setRecipes([...recipes, data.recipe]);    
+        }
+ 
+    } catch (e) {
+      console.log("There was an error, sorry", e)
+    }
+    setShowNewRecipeForm(false)
+    setNewRecipe({
+      title: "",
+      ingredients: "",
+      instructions: "",
+      servings: 1,
+      description: "",
+      image_url: "https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    });
+  };
+
+ // useEffect (()=> {
+ //   fetchQuotes();
+ // }, []);
+
+ // useEffect (() => {
+ //   window.localStorage.setItem("favoriteQuotes", JSON.stringify(favoriteQuotes))
+//  },[favoriteQuotes]);
+
   return (
     <div className='recipe-app'>
        <Header showRecipeForm={showRecipeForm} />
        {showNewRecipeForm && 
-        <NewRecipeForm hideRecipeForm={hideRecipeForm} onUpdateForm={onUpdateForm} newRecipe={newRecipe} />
+        <NewRecipeForm hideRecipeForm={hideRecipeForm} onUpdateForm={onUpdateForm} newRecipe={newRecipe} handleNewRecipe={handleNewRecipe} />
        }
       {selectedRecipe && (
       <RecipeFull 
@@ -79,7 +118,7 @@ function App() {
         handleUnselectRecipe={handleUnselectRecipe} 
       />
       )}
-    {selectedRecipe === null && (
+    {selectedRecipe === null && !showNewRecipeForm && (
     <div className="recipe-list">
       {recipes.map(recipe => (
         <RecipeExcerpt 
--- src/components/NewRecipeForm.js
+++ src/components/NewRecipeForm.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const NewRecipeForm = ({newRecipe, hideRecipeForm, NewRecipeForm, onUpdateForm}) => { 
+const NewRecipeForm = ({newRecipe, hideRecipeForm, NewRecipeForm, onUpdateForm, handleNewRecipe}) => { 
   return (
 <div className='recipe-details'>
       <div className='recipe-form'>
         <h2>New Recipe</h2>
         <button className='cancel-button' onClick={hideRecipeForm}>Cancel</button>
  
-        <form>
+        <form onSubmit={(e) => handleNewRecipe(e, newRecipe)}>
           <label>Title</label>
           <input type='text' name='title' value={newRecipe.title} onChange={(e) => onUpdateForm(e)} required />
  
@@ -27,7 +27,7 @@ const NewRecipeForm = ({newRecipe, hideRecipeForm, NewRecipeForm, onUpdateForm})
           <textarea name='description' value={newRecipe.description} onChange={(e) => onUpdateForm(e)} required />
  
           <label>Image</label>
-          <input type='text' name='image_url' value={newRecipe.image} onChange={(e) => onUpdateForm(e)} required />
+          <input type='text' name='image_url' value={newRecipe.image_url} onChange={(e) => onUpdateForm(e)} required />
  
           <label>Servings</label>
           <input type='number' name='servings' value={newRecipe.servings} onChange={(e) => onUpdateForm(e)} required />
