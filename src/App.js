f355d18c435f416e4844f32eb3780617f709885f
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2024-04-01 13:39:18 +0200

    Troubleshooting the handleUpdateRecipe function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
-import "./App.css";
-import { useState } from "react";
-import { useEffect } from "react";
 import RecipeExcerpt from "./components/RecipeExcerpt";
 import RecipeFull from "./components/RecipeFull";
 import NewRecipeForm from "./components/NewRecipeForm";
+import "./App.css";
 
 function App() {
    const [recipes, setRecipes] = useState([])
    const [selectedRecipe, setSelectedRecipe] = useState(null)
+   const [showNewRecipeForm, setShowNewRecipeForm] = useState (false)
+   
    const [newRecipe, setNewRecipe] = useState(
     {
       title: "",
@@ -19,28 +20,24 @@ function App() {
       description: "",
       image_url: "https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" //default
     });
-    const [showNewRecipeForm, setShowNewRecipeForm] = useState (false)
    
    
-
-   const fetchAllRecipes = async () => {
+   useEffect (() => {
+    const fetchAllRecipes = async () => {
       try {
         const response = await fetch ('/api/recipes');
-        const result = await response.json();
       
         if (response.ok) {
+          const result = await response.json();
           setRecipes(result);
     } 
        else {
           console.log("Not able to retrieve recipe at this time. Please try again later.")
         }
       } catch (error) {
-        console.error("An error accord while fetching recipes,", error);
+        console.error("An error occured while fetching recipes,", error);
       }
       };
-
-
-  useEffect (() => {
         fetchAllRecipes();
       },[]);
 
@@ -73,70 +70,72 @@ function App() {
   };  
   
 
-  const handleNewRecipe = async (e, newFormRecipe) => {
+  const handleNewRecipe = async (e, newRecipe) => {
     e.preventDefault()
     try {
-   //   setLoading(true)
-      const response = await fetch('api/recipes', {
+      const response = await fetch('/api/recipes', {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
         },
-        body: JSON.stringify(newFormRecipe)
+        body: JSON.stringify(newRecipe)
       });
         if (response.status === 200) {
            const data =  await response.json()
            setRecipes([...recipes, data.recipe]);    
-        }
- 
-    } catch (e) {
-      console.log("The form did not work, sorry. No new recipe for you!", e)
-    }
-    setShowNewRecipeForm(false)
-    setNewRecipe({
-      title: "",
-      ingredients: "",
-      instructions: "",
-      servings: 1,
-      description: "",
-      image_url: "https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-    });
-  };
+           console.log("Recipe added successfully!")
+           
+           setShowNewRecipeForm(false)
+           setNewRecipe({
+             title: "",
+             ingredients: "",
+             instructions: "",
+             servings: 1,
+             description: "",
+             image_url: "https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+           });
+          } else{
+            console.error("Oops - could not add recipe!");
+      
+          }
+          } catch (e) {
+            console.error("The form did not work, sorry. No new recipe for you!")
+          }
+       };
 
   const handleUpdateRecipe = async (e, selectedRecipe) => {
     e.preventDefault()
     const { id } = selectedRecipe;
     try {
-      const response = await fetch(`api/recipes/${id}`, {
+      const response = await fetch(`/api/recipes/${id}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json"
         },
         body: JSON.stringify(selectedRecipe)
       });
+
         if (response.status === 200) {
-           const data =  await response.json()
-           setRecipes(recipes.map((recipe) => {
-           return recipe.id === selectedRecipe.id ?
-            data.recipe : recipe
-            })
-         ); 
-           console.log("Recipe successfully updated")
-        };
- 
-    } catch (e) {
-      console.log("The recipe did not update, sorry.", e)
+           const data =  await response.json();
+           setRecipes(
+           recipes.map((recipe) => {
+            if (recipe.id === id) {
+             return data.recipe;
+            }
+          return recipe;
+        })
+        );
+          console.log("Recipe successfully updated");
+        } else {
+          console.error("Failed to update recipe")
+        }
+    } catch (error) {
+      console.error("The recipe did not update, sorry.")
     }
+
     setSelectedRecipe(null)
-    setNewRecipe({
-      title: "",
-      ingredients: "",
-      instructions: "",
-      servings: 1,
-      description: "",
-      image_url: "https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-    });
   };
+
 
   return (
     <div className='recipe-app'>
